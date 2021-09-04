5c05d60127d7adf0e7b03f555e3710afbe3baf45
Vaibhav Jain <82175956+vaibhavHere@users.noreply.github.com>
2021-09-04 17:16:16 +0530

    Added fav feature
    You can now have your favourite superheroes stored to locally.

--- app.js
+++ app.js
@@ -1,106 +1,111 @@
-FavId = []
-search.addEventListener('change', async () => {
-    console.log(search.value)
-    output.innerHTML = ''
-    result = await getData();
-    for (let i = 0; i < result.length; i++) {
-        checkFav = `<i class="far fa-heart"></i>`
-        if (FavId.includes(result[i].id)) {
-            checkFav = checkFav.replace('far', 'fas')
-        }
-        console.log(checkFav)
-        text = `<div heroid=${result[i].id} count=${i} class="card">
-<div class="heroimg">
-    <img src="${result[i].image.url}" alt="${result[i].name}">
-</div>
-<div class="text">
-    <h1 class="name">
-        <h1>${result[i].name}</h1>
-        <p>A.K.A ${result[i].biography["full-name"]}</p>
-        <p>${result[i].appearance.gender} | ${result[i].appearance.race}</p>
-</div>
-<div class="btns">
-    <button>More..</button>
-    ${checkFav}
-</div>
-</div>`
-        output.insertAdjacentHTML('beforeend', text);
+const output = document.getElementById('output'),
+    outputContainer = document.getElementById('outputContainer'),
+    favTitle = document.getElementById('favTitle')
+var FavId,tempFav;
+
+if (localStorage.getItem('FavId'))
+FavId = localStorage.getItem('FavId');
+else
+localStorage.setItem('FavId', JSON.stringify([]));
+FavId = JSON.parse(localStorage.getItem('FavId'))
+
+function copy(element){
+    temp=[];
+    for(i=0;i<element.length;i++)
+    temp.push(element[i])
+    return temp;
+}
+
+
+fav.onclick = () => {
+    reset();
+    search.value=''
+    tempFav=copy(FavId)
+    displayCards(FavId, output, "fav")
+    if (FavId == '')
+        favTitle.innerHTML = "You don't have any Favs :("
+    else
+        favTitle.innerHTML = "Your Favs :)"
+
+}
+
+search.onchange = async () => {
+    reset();
+    try {
+        result = await getData();
+    } catch (error) {
+        output.insertAdjacentHTML('beforeend', error);
     }
+    if (result)
+        displayCards(result, output);
+    else
+        output.insertAdjacentHTML('beforeend', `<div class="oops"><h1>Not found :(</h1><img src='assets/img/oops.png'></div>`);
+    console.log(result)
+}
+
+// FUNCTIONS
+getData = async () => (await ((await fetch('https://www.superheroapi.com/api.php/3123593447863947/search/' + search.value)).json())).results;
+
+isFav = result => {
+    if(FavId!=''){
+    for (j = 0; j < FavId.length; j++) {
+        if (FavId[j].id == result.id)
+            return `<i class="fas fa-heart"></i>`
+    }
+}
+else
+    return `<i class="far fa-heart"></i>`
+}
+
+reset = () => output.innerHTML = details.innerHTML = favTitle.innerHTML = ''
+
+displayCards = (element, target, mode) => {
+    reset();
+    for (let i = 0; i < element.length; i++) {
+        checkFav = isFav(element[i]);
+        text = `<div heroid=${element[i].id} count=${i} class="card"><div class="heroimg"><img src="${element[i].image.url}" alt="${element[i].name}"></div><div class="text"><h1 class="name"><h1>${element[i].name}</h1><p>A.K.A ${element[i].biography["full-name"]}</p><p>${element[i].appearance.gender} | ${element[i].appearance.race}</p></div><div class="btns"><button>More..</button>${checkFav}</div></div>`
+        target.insertAdjacentHTML('beforeend', text);
+    }
+
     hearticon = Object.values(document.querySelectorAll('.btns i'))
-    readMore = Object.values(document.querySelectorAll('.btns button'))
-    console.log(readMore)
-    for (i = 0; i < readMore.length; i++) {
-        readMore[i].addEventListener('click', () => {
-        output.innerHTML=''
-            console.log(event.path[2].getAttribute('heroid'))
-            console.log(result);
-            index=event.path[2].getAttribute('count');
-            // if (event.target.className == 'far fa-heart') {
-            //     FavId.push(event.path[2].getAttribute('heroid'))
-            //     event.target.className = 'fas fa-heart'
-            // } else {
-            //     FavId.pop(event.path[2].getAttribute('heroid'))
-            //     event.target.className = 'far fa-heart'
-            // }
-            console.log(result[index])
-            detail=`<div class="left-image">
-            <img src="${result[index].image.url}">
-        </div>
-        <div class="text">
-            <h1 name>${result[index].name}<span class='btns'><i class="far fa-heart"></i></span></h1>
-            <p>A.K.A ${result[index].biography["full-name"]}</p>
-            <h1>Powerstats</h1>
-            <div>
-                <p class='strength'>Intelligence </p><meter min='0' max='100' value='${result[index].powerstats.intelligence}' low='50'></meter>
-            </div>
-            <div>
-                <p class='strength'>Strength </p><meter min='0' max='100' value='${result[index].powerstats.strength}' low='50'></meter>
-            </div>
-            <div>
-                <p class='strength'>Speed </p><meter min='0' max='100' value='${result[index].powerstats.speed}' low='50'></meter>
-            </div>
-            <div>
-                <p class='strength'>Duarbility </p><meter min='0' max='100' value='${result[index].powerstats.durability}' low='50'></meter>
-            </div>
-            <div>
-                <p class='strength'>Power </p><meter min='0' max='100' value='${result[index].powerstats.power}' low='50'></meter>
-            </div>
-            <div>
-                <p class='strength'>Combat </p><meter min='0' max='100' value='${result[index].powerstats.combat}' low='50'></meter>
-            </div>
-            <h1>Add-ons</h1>
-            <b>Aliases</b>
-            </p>${result[index].biography.aliases}</p>
-            <p><b>First Appearance</b> World's Finest #111 (Augu${result[index].biography["first-appearance"]}</p>
-            <p><b>Publisher</b> ${result[index].biography["publisher"]}</p>
-            <p>${result[index].appearance.gender} - ${result[index].appearance.race}</p>
-            <p><b>Height</b> ${result[index].appearance.height[0]} - ${result[index].appearance.height[1]}</p>
-            <p><b>Weight</b> ${result[index].appearance.weight[0]} - ${result[index].appearance.weight[1]}</p>
-            <p><b>Eye Color</b> ${result[index].appearance["eye-color"]=='-'?"Unavailable":result[index].appearance["eye-color"]}</p>
-            <p><b>Hair Color</b> ${result[index].appearance["hair-color"]=='-'?"Unavailable":result[index].appearance["hair-color"]}</p>
-            <p><b>League</b>
-               ${result[index].connections["group-affiliation"]}
-            </p>
-        </div>`
-        console.log(result[index].connections)
-            details.insertAdjacentHTML('beforeend',detail)
-        })
-    }
-    console.log(readMore)
     for (i = 0; i < hearticon.length; i++) {
-        hearticon[i].addEventListener('click', () => {
-            console.log(event.path[2].getAttribute('heroid'))
+        hearticon[i].onclick = () => {
             if (event.target.className == 'far fa-heart') {
-                FavId.push(event.path[2].getAttribute('heroid'))
+                FavId.push(result[event.path[2].getAttribute('count')])
                 event.target.className = 'fas fa-heart'
+                localStorage.setItem('FavId', JSON.stringify(FavId))
             } else {
-                FavId.pop(event.path[2].getAttribute('heroid'))
+                id = event.path[2].getAttribute('heroid')
+                for (i = 0; i < FavId.length; i++) {
+                    if (FavId[i].id == id)
+                        FavId.splice(i, 1)
+                }
+                localStorage.setItem('FavId', JSON.stringify(FavId))
                 event.target.className = 'far fa-heart'
             }
-            console.log(FavId)
-        })
+        }
     }
-})
-async function getData() {
-    return (await ((await fetch('https://www.superheroapi.com/api.php/3123593447863947/search/' + search.value)).json())).results;
+
+    // readMore();
+    readMore = Object.values(document.querySelectorAll('.btns button'))
+
+    for (i = 0; i < readMore.length; i++) {
+        readMore[i].onclick = () => {
+            reset();
+            index = event.path[2].getAttribute('count')
+            if (mode == 'fav'){
+                displayDetail(tempFav[index])
+            }
+            else
+                displayDetail(result[index])
+        }
+    }
+}
+
+
+displayDetail = (element) => {
+    text = `<div class="left-image"><img src="${element.image.url}">
+        </div><div class="text"><h1 name>${element.name}</h1>
+            <p>A.K.A ${element.biography["full-name"]}</p><h1>Powerstats</h1><div><p class='strength'>Intelligence </p><meter min='0' max='100' value='${element.powerstats.intelligence}' low='50'></meter></div><div><p class='strength'>Strength </p><meter min='0' max='100' value='${element.powerstats.strength}' low='50'></meter></div><div><p class='strength'>Speed </p><meter min='0' max='100' value='${element.powerstats.speed}' low='50'></meter></div><div><p class='strength'>Duarbility </p><meter min='0' max='100' value='${element.powerstats.durability}' low='50'></meter></div><div><p class='strength'>Power </p><meter min='0' max='100' value='${element.powerstats.power}' low='50'></meter></div><div><p class='strength'>Combat </p><meter min='0' max='100' value='${element.powerstats.combat}' low='50'></meter></div><h1>Add-ons</h1><b>Aliases</b></p>${element.biography.aliases}</p><p><b>First Appearance</b> World's Finest #111 (Augu${element.biography["first-appearance"]}</p><p><b>Publisher</b> ${element.biography["publisher"]}</p><p>${element.appearance.gender} - ${element.appearance.race}</p><p><b>Height</b> ${element.appearance.height[0]} - ${element.appearance.height[1]}</p><p><b>Weight</b> ${element.appearance.weight[0]} - ${element.appearance.weight[1]}</p><p><b>Eye Color</b> ${element.appearance["eye-color"]=='-'?"Unavailable":element.appearance["eye-color"]}</p><p><b>Hair Color</b> ${element.appearance["hair-color"]=='-'?"Unavailable":element.appearance["hair-color"]}</p><p><b>League</b>${element.connections["group-affiliation"]}</p></div>`
+    details.insertAdjacentHTML('beforeend', text)
 }
